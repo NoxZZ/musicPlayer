e8e6e0bf00f5f506209a89897e5b813d7a980d2e
NoxZZ <33574795+NoxZZ@users.noreply.github.com>
2019-02-13 21:03:54 +0530

    a basic script in python for playing music

---
+++ musicPlayer.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+a  script file.
+"""
+import webbrowser as wb
+choice = 0
+def mood(choice):
+    while True:
+            if choice == 1:
+                wb.open('https://www.youtube.com/watch?v=-boKk8uhmcY&list=PLKe9CVqEUDMGhEM42kKWsVt0SScAE1r0X')
+            elif choice == 5:
+                wb.open('https://www.youtube.com/watch?v=RgKAFK5djSk&list=PLKe9CVqEUDME-e6ulWPdlV7vt3T5PaHi5')
+            elif choice == 2:
+                wb.open('https://www.youtube.com/playlist?list=PLKe9CVqEUDMEYvAy57HkCNfVBprD18cA-')
+            elif choice == 3:
+                wb.open('https://www.youtube.com/playlist?list=PLKe9CVqEUDMEcpW3Zy2KJWTcPpTGtKAck')
+            elif choice == 4:
+                wb.open('https://www.youtube.com/playlist?list=PLKe9CVqEUDMH77Bk07v8esKG5L83UOFjH')
+            else :
+                print('please choose the options from the list !')
+choice = int(input('choose the type of music you want : \n1) mix \n2) edm \n3) pop \n4) rock  \n5) chill\nEnter your input:'))        
+mood(choice)
